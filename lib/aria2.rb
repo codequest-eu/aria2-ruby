b2ad2cb80bfc5a2bc421334f04b1fa87a869cdf1
Tomasz Warkocki <warkocz@gmail.com>
2015-02-09 10:08:46 +0100

    cleanup

--- lib/aria2.rb
+++ lib/aria2.rb
@@ -1,230 +1,186 @@
-require "aria2/version"
+require 'aria2/version'
+require 'rest_client'
 
 module Aria2
 
-	class Downloader
+  class Downloader
 
-		require 'open-uri'
-		require 'json'
-		require 'base64'
-		require 'cgi'
-		require 'net/http'
+    require 'open-uri'
+    require 'json'
+    require 'base64'
+    require 'cgi'
+    require 'net/http'
 
-		def self.new(host = 'localhost', port = 6800, token = '')
-			@host = host
-			@port = port
-			@token = token
-			self
-		end
+    def initialize(params)
+      @host = params[:host] || 'localhost'
+      @port = params[:port] || 6800
+      @token = params[:token] || ''
+    end
 
-		def self.check
-			begin
-				self.rpc_call('getGlobalStat', [])
-				true
-			rescue
-				false
-			end
-		end
+    def check
+      rpc('getGlobalStat', [])
+    end
 
-		def self.remove(gid)
-			self.rpc_call('remove', [gid]) == gid
-			status
-		end
+    def remove(gid)
+      rpc('remove', [gid])
+    end
 
-		def self.forceRemove(gid)
-			status = self.rpc_call('forceRemove',[gid]) == gid
-			status
-		end
+    def forceRemove(gid)
+      rpc('forceRemove', [gid])
+    end
 
-		def self.pause(gid)
-			status = self.rpc_call('pause', [gid]) == gid
-			status
-		end
+    def pause(gid)
+      rpc('pause', [gid])
+    end
 
-		def self.pauseAll()
-			status = self.rpc_call('pauseAll', [])
-			status
-		end
+    def pauseAll()
+      rpc('pauseAll', [])
+    end
 
-		def self.forcePause(gid)
-			status = self.rpc_call('forcePause', [gid]) == gid
-			status
-		end
+    def forcePause(gid)
+      rpc('forcePause', [gid])
+    end
 
-		def self.forcePauseAll()
-			status = self.rpc_call('forcePauseAll', [])
-			status
-		end
+    def forcePauseAll()
+      rpc('forcePauseAll', [])
+    end
 
-		def self.unPause(gid)
-			status = self.rpc_call('unPause', [gid]) == gid
-			status
-		end
+    def unPause(gid)
+      rpc('unPause', [gid])
+    end
 
-		def self.unPauseAll()
-			status = self.rpc_call('unPauseAll', [])
-			status
-		end
+    def unPauseAll()
+      rpc('unPauseAll', [])
+    end
 
+    def getUris(gid)
+      rpc('getUris', [gid])
+    end
 
-		def self.download(url, path)
-			path = File.expand_path(path)
-			self.rpc_call('addUri', [[url], {
-				'dir' => File.dirname(path), 
-				'out' => File.basename(path),
-				'allow-overwrite' => 'true'
-			}])
-		end
+    def getFiles(gid)
+      rpc('getFiles', [gid])
+    end
 
+    def getPeers(gid)
+      rpc('getPeers', [gid])
+    end
 
-		def self.getUris(gid) 
-			uris = self.rpc_call('getUris', [gid]) == gid
-			uris
+    def getServers(gid)
+      rpc('getServers', [gid])
+    end
 
-		end
+    def getOption(gid)
+      rpc('getOption', [gid])
+    end
 
-		def self.getFiles(gid)
-			files = self.rpc_call('getFiles', [gid]) == gid
-			files
-		end
+    def changeOption(gid, options)
+      rpc('changeOption', [gid, options])
+    end
 
-		def self.getPeers(gid)
-			peers = self.rpc_call('getPeers', [gid]) == gid
-			peers
-		
-		end
+    def purgeDownloadResult()
+      rpc('purgeDownloadResult')
+    end
 
-		def self.getServers(gid)
-			servers = self.rpc_call('getServers', [gid]) == gid
-			servers
-		end
+    def removeDownloadResult(gid)
+      rpc('removeDownloadResult', [gid])
+    end
 
-		def self.getOption(gid)
-			option = self.rpc_call('getOption', [gid]) == gid
-			option
-		end
+    def getVersion()
+      rpc('getVersion', [])
+    end
 
-		def self.purgeDownloadResult()
-			status = self.rpc_call('purgeDownloadResult')
-			status
-		end
+    def getSessionInfo()
+      rpc('getSessionInfo', [])
+    end
 
-		def self.removeDownloadResult(gid)
-			status = self.rpc_call('removeDownloadResult', [gid]) == gid
-			status
-		end
+    def shutdown()
+      rpc('shutdown', [])
+    end
 
-		def self.getVersion()
-			version = self.rpc_call('getVersion', [])
-			version
+    def forceShutdown()
+      rpc('forceShutdown', [])
+    end
 
-		end
+    def saveSession()
+      rpc('saveSession', [])
+    end
 
-		def self.getSessionInfo()
-			sessionId = self.rpc_call('getSessionInfo', [])
-			sessionId
+    def addTorrent(torrent)
+      rpc('addTorrent', [torrent])
+    end
 
-		end
+    def addTorrentFile(filename)
+      torrent = Base64.encode64(File.open(filename, "rb").read)
+      addTorrent(torrent)
+    end
 
-		def self.shutdown()
-			status = self.rpc_call('shutdown', [])
-			status
-		end
+    def getActive()
+      rpc('tellActive', [])
+    end
 
-		def self.forceShutdown()
-			status = self.rpc_call('forceShutdown', [])
-			status
-		end
+    def query_status(gid)
+      status = rpc('tellStatus', [gid, [
+                                         'status',
+                                         'totalLength',
+                                         'completedLength',
+                                         'downloadSpeed',
+                                         'errorCode',
+                                         'infoHash',
+                                         'numSeeders'
+                                     ]])
 
-		def self.saveSession()
-			status = self.rpc_call('saveSession', [])
-			status
-		end
+      status['totalLength'] = status['totalLength'].to_i
+      status['completedLength'] = status['completedLength'].to_i
+      status['downloadSpeed'] = status['downloadSpeed'].to_i
+      status['errorCode'] = status['errorCode'].to_i
+      status['infoHash'] = status['infoHash']
+      status['numSeeders'] = status['numSeeders'].to_i
 
-		def self.addTorrent(torrent)
-			gid = self.rpc_call('addTorrent', [torrent]) == torrent
-			gid
-		end
+      status['progress'] = status['totalLength'] == 0 ?
+          0 :
+          status['completedLength'].to_f / status['totalLength'].to_f
 
-		def self.addTorrentFile(filename)
-			torrent = Base64.encode64(File.open(filename, "rb").read)
-			gid = self.addTorrent(torrent)
-			gid
-		end
+      status['remainingTime'] = status['downloadSpeed'] == 0 ?
+          0 :
+          (status['totalLength'] - status['completedLength']).to_f / status['downloadSpeed']
 
-		def self.getActive()
-			status = self.rpc_call('tellActive', [])
-			status	
-		end
+      status
+    end
 
-		def self.query_status(gid)
-			status = self.rpc_call('tellStatus', [gid, [
-				'status', 
-				'totalLength', 
-				'completedLength', 
-				'downloadSpeed', 
-				'errorCode'
-			]])
+    private
 
-			status['totalLength'] = status['totalLength'].to_i
-			status['completedLength'] = status['completedLength'].to_i
-			status['downloadSpeed'] = status['downloadSpeed'].to_i
-			status['errorCode'] = status['errorCode'].to_i
+    def post(url, params = {})
+      uri = URI.parse(url)
+      response = RestClient.post(url, params.to_json, :content_type => :json, :accept => :json)
+      {
+          'code' => response.code.to_i,
+          'body' => response.body
+      }
+    end
 
-			status['progress'] = status['totalLength'] == 0 ? 
-				0 :
-				status['completedLength'].to_f / status['totalLength'].to_f
+    def rpc_path
+      "http://#{@host}:#{@port}/jsonrpc"
+    end
 
-			status['remainingTime'] = status['downloadSpeed'] == 0 ?
-				0 :
-				(status['totalLength'] - status['completedLength']).to_f / status['downloadSpeed']
+    def rpc(method, params)
+      method = "aria2.#{method}"
+      id = 'ruby-aria2'
+      params_encoded = params #Base64.encode64(JSON.generate(params))
+      if @token != '' then
+        response = post("#{rpc_path}", {'token' => @token, 'method' => method, 'id' => id, 'params' => params_encoded})
+      else
+        response = post("#{rpc_path}", {'method' => method, 'id' => id, 'params' => params_encoded})
+      end
+      answer = JSON.parse(response['body'])
 
-			status
-		end
+      if response['code'] == 200
+        answer['result']
+      else
+        raise "AriaDownloader error #{answer['error']['code'].to_i}: #{answer['error']['message']}"
+      end
+    end
 
-		
-
-		private
-
-			def self.get(url, params = {})
-				uri = URI.parse(url)
-
-				uri.query = URI.encode_www_form(params)
-
-				http = Net::HTTP.new(uri.host, uri.port)
-				request = Net::HTTP::Get.new(uri.request_uri)
-
-				response = http.request(request)
-
-				{
-					'code' => response.code.to_i, 
-					'body' => response.body
-				}
-			end
-
-			def self.rpc_path
-				"http://#{@host}:#{@port}/jsonrpc"
-			end
-
-			def self.rpc_call(method, params)
-				method = "aria2.#{method}"
-				id = 'ruby-aria2'
-				params_encoded = Base64.encode64(JSON.generate(params))
-				if @token != '' then
-					response = get("#{self.rpc_path}", {'token' => @token, 'method' => method, 'id' => id, 'params' => params_encoded})
-				else
-					response = get("#{self.rpc_path}", {'method' => method, 'id' => id, 'params' => params_encoded})
-				end
-				answer = JSON.parse(response['body'])
-
-				if response['code'] == 200
-					answer['result']
-				else
-					raise "AriaDownloader error #{answer['error']['code'].to_i}: #{answer['error']['message']}"
-				end
-			end
-
-	end
+  end
 
 end
 
